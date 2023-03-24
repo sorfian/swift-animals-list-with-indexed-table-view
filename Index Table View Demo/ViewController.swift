3df7873774c67abcd2442ec176dbab48b76e2ef6
Sorfian <sorfian.tkj@gmail.com>
2023-03-24 14:41:21 +0800

    Initial Commit

---
+++ Index Table View Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Index Table View Demo
+//
+//  Created by Sorfian on 24/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
